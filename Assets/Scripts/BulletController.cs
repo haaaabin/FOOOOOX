b5c936f229be6268e6888187e31add6a2a317383
choihabin <chabin7181@gmail.com>
2024-06-26 02:43:06 +0900

    refactor : 동일한 기능을 가진 Bullet 스크립트를 BulletController.cs로 통합
    - BulletCtrl과 MonBulletCtrl을 BulletController로 통합
    - InitializeBullet 메서드를 생성하여 플레이어와 몬스터 스크립트에 적절한 매개변수를 전달하여 사용

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Vector3 direction = Vector3.right;
+    private float moveSpeed = 10.0f;
+    private float lifeTime = 0.7f; // 기본 수명
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    
+    private void Update()
+    {
+        transform.position += direction * Time.deltaTime * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void InitializeBullet(Vector3 startPos, Vector3 dir, float speed, float bulletLifeTime = 0.7f)
+    {
+        direction = dir;
+        transform.position = new Vector3(startPos.x, startPos.y, 0.0f);
+        moveSpeed = speed;
+        lifeTime = bulletLifeTime;
+        Destroy(gameObject, lifeTime);
+    }
+}
