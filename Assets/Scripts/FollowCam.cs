9f06d99f5431f22a56556bfe55dd7871b1aedf5e
choihabin <chabin7181@gmail.com>
2024-12-25 00:57:09 +0900

    Feat : 시네머신 카메라 이동 제한 영역
    - CinemachineConfinder2D를 사용하여 카메라 이동 영역 제한

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -1,13 +1,16 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FollowCam : MonoBehaviour
 {
     public CinemachineVirtualCamera vCam;
+    public CinemachineConfiner2D cinemachineConfiner2D;
 
     // Start is called before the first frame update
     void Start()
     {
         vCam.Follow = PlayerCtrl.Instance.transform;
+        cinemachineConfiner2D.m_BoundingShape2D = GameObject.Find("Cam Border").GetComponent<PolygonCollider2D>();
     }
 }
